3249ab390c9a49426e26e312627298c2e4e9d77d
DreamFor <dreamforway@qq.com>
2025-02-25 23:56:02 +0800

    feat(11): ref创建

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -3,15 +3,22 @@
         <h2>name:{{ name }}</h2>
         <h2>age:{{ age }}</h2>
         <button @click="showTel">展示电话</button>
+        <button @click="changeName">修改名称</button>
     </div>
 </template>
 
 <script lang="ts" setup name="Person">
-    let name = ' 张三';
+    import { ref } from 'vue';
+
+
+    let name = ref('张三');
     let age = 18;
     let tel = '1312223232323'
     function showTel(){
         alert(tel);
+    }
+    function changeName(){
+        name.value = 'Zhang-San';
     }
 </script>
 
@@ -21,5 +28,8 @@
         box-shadow: 0 0 10px;
         border-radius: 10px;
         padding: 20px;
+    }
+    button{
+        margin: 0px 5px;
     }
 </style>
