165c5a68f79f377ebdfecc970ed22dbe4eb6b3f7
DreamFor <dreamforway@qq.com>
2025-03-06 12:14:44 +0800

    feat(17): watch监视 情况1 基本类型

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,44 +1,24 @@
 <template>
     <div class="person">
-       姓：<input type="text" v-model="fName"> <br>
-       名：<input type="text" v-model="lName"> <br>
-       全名：<span>{{ fullName }}</span> <br>
-       <button @click="changeFullNameLS">更改名称为Li-Si</button>
-       <button @click="changeFullNameZS">更改名称为Li-Si</button>
+        <!-- 1. 基本类型 -->
+         <h2>Sum:{{ sum }}</h2>
+         <button @click="changeSum">点我Sum+1</button>
+         
     </div>
 </template>
 
 <script lang="ts" setup>
-    defineOptions({
-        name:'Person'
-    })
-    import { computed, ref } from 'vue';
-    let fName = ref('A');
-    let lName = ref('B');
+import { reactive, ref, watch } from 'vue';
+    /* 情况1：基本类型 */
+    let sum = ref(1);
 
-    /* 此种写法为只读，无法再次操作 */
-    // let fullName = computed(()=>{
-    //     return fName.value + '-' + lName.value
-    // });
-
-    /* 本质是传入一个带有set和get方法的对象，通过对象中的方法进行更改，避免只读无法更改的情况 */
-    let fullName = computed({
-        get(){
-            return fName.value + '-' + lName.value;
-        },
-        /* 传入内容为Li-Si，即fullName.value = 'Li-Si'此处设置值 */
-        set(val){
-            const [f, l] = val.split('-');
-            fName.value = f;
-            lName.value = l;
-        }
-    });
-
-    function changeFullNameLS(){
-        fullName.value = 'Li-Si';
-    }function changeFullNameZS(){
-        fullName.value = 'Zhang-San';
+    function changeSum(){
+        sum.value ++ ;
     }
+
+    watch(sum, (newVal, oldVal)=>{
+        console.log('Sum发生变化了', newVal, oldVal);
+    })
 </script>
 
 <style scoped>
