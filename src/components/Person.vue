01825afc0066fbcdae15b085b337ed3aec006695
DreamFor <dreamforway@qq.com>
2025-03-07 15:25:07 +0800

    feat(22): watchEffect的使用

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,65 +1,40 @@
 <template>
     <div class="person">
-         <h2>{{ car.brand }}售价：{{ car.price }}</h2>
-         <button @click="changeCarBrand">点我更改车型</button>
-         <button @click="changeCarPrice">点我更改价格</button>
-         <button @click="changeCar">点我更改整台车</button>
-         
+        <h2>监控{{ f }} + {{ s }} = {{ sum }}</h2>
+        <button @click="changeF">改变第一个</button>
+        <button @click="changeS">改变第二个</button>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { reactive, ref, watch } from 'vue';
-    
-    /* 对象 reactive默认开启深度，ref不默认*/
-    let car = ref({
-        brand:'宝马',
-        price:100_000,
-    })
+import { computed, ref, watchEffect } from 'vue';
 
-    function changeCarBrand(){
-        car.value.brand = '奇瑞';
-    }
-
-    function changeCarPrice(){
-        car.value.price ++;
-    }
-
-    /* 是一种替换行为，所以，未开启深度监视也可以被监视到 */
-    function changeCar(){
-        car.value = {
-            brand:'长安',
-            price:10000,
+   let f = ref(0);
+   let s = ref(20);
+   let sum = computed({
+        get(){
+            return f.value + s.value;
+        },
+        set(val){
+            console.log(val);
         }
-    }
-    watch(car, (newVal, oldVal)=>{
-        console.log('Car发生了变化', newVal, oldVal);
-    })
+   })
 
-    
-    // let car = reactive({
-    //     brand:'宝马',
-    //     price:100_000,
-    // })
-
-    // /* 补充:Reactive无法直接更换整个对象，但可以直接更改内部对象的值 */
-    // /* function changeCar(){
-    //     car = {brand:'长安',
-    //         price:1000,
-    //     }
-    // } */
-
-    // function changeCarBrand(){
-    //     car.brand = '奇瑞';
-    // }
-
-    // function changeCarPrice(){
-    //     car.price += 100;
-    // }
-
-    // watch(car, (newVal, oldVal)=>{
-    //     console.log('Car发生了变化', newVal, oldVal);
-    // }, {deep:false})
+   function changeF(){
+        f.value += 10;
+   }function changeS(){
+        s.value += 10;
+   }
+   
+   watchEffect((val)=>{
+        if(f.value >= 60){
+            console.log('f超过60了');
+        }
+        if(s.value >= 100){
+            console.log('s大于等于100了');
+        }
+        console.log(val);
+   })
 </script>
 
 <style scoped>
