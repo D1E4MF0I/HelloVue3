a09bd5b5e9bc006dc42f0926d352ab4955dbe5ba
DreamFor <dreamforway@qq.com>
2025-03-07 16:53:09 +0800

    feat(25): props的使用

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-    <Person />
+    <Person a="哈哈" :list="personList"/>
 </template>
 
 <script lang="ts" setup>
     defineOptions({name:'App'})
+import { reactive } from 'vue';
     import Person from './components/Person.vue'
+import type { Persons } from './types';
+
+    let personList = reactive<Persons>([
+        {id:'1', name:'1', age:18},
+        {id:'2', name:'2', age:23},
+        {id:'3', name:'3', age:52},
+    ])
     
 </script>
 
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,31 +1,37 @@
 <template>
     <div class="person">
-        ???
+        <h2>{{ a }}</h2>
+        <ul>
+            <li v-for="item in list" :key="item.id">
+                {{ item.name }}:{{ item.age }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script lang="ts" setup>
-    import {type PersonInter, type Persons} from '@/types'
-    // let person:PersonInter = {
-    //     id: '123123',
-    //     name: '123123',
-    //     age: 100,
-    // }
-    let personList:Persons = [
-    {
-        id: '1',
-        name: '1',
-        age: 1,
-    },{
-        id: '2',
-        name: '2',
-        age: 100,
-    },{
-        id: '3',
-        name: '3',
-        age: 10,
-    },
-    ]
+    import { type Persons } from '@/types'
+    import {} from 'vue'
+
+    /* 接收a */
+    // defineProps(['a'])
+
+    /* 接收并保存起来 */
+    // let x = defineProps(['a'])
+    // console.log(x, x.a);
+
+    /* 只接收list */
+    // defineProps(['list'])
+
+    /* 接收list+限制类型 */
+    // defineProps<{list:Persons}>()
+
+    // 接收list+限制类型+限制必要性+指定默认值
+    withDefaults(defineProps<{list?:Persons}>(), {
+        list:()=>[{id:'1', name:'1', age:18},{id:'2', name:'2', age:23}, {id:'3', name:'3', age:52},]
+    })
+    
+    
 </script>
 
 <style scoped>
