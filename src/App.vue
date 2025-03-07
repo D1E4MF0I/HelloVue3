561512ce53a2f849b3a5b3216bcdcb6d69f2ad9c
DreamFor <dreamforway@qq.com>
2025-03-07 16:22:29 +0800

    feat(25): TS 接口 泛型 自定义类型

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,11 @@
 <template>
-    <h2>测试</h2>
-    <button @click="showPerson">展示Person组件</button>
-    <Person ref="person"/>
+    <Person />
 </template>
 
 <script lang="ts" setup>
     defineOptions({name:'App'})
-import { ref } from 'vue';
     import Person from './components/Person.vue'
-
-    let person = ref();
-
-    function showPerson(){
-        console.log(person.value);
-        let index = person.value;
-        console.log(index.a, index.b, index.c);
-        
-        /* 展示defineExpose中传递的参数，子组件自定义可传递给父组件的内容
-                {
-            "a": 0,
-            "b": 1,
-            "c": 2
-        }
-        */
-    }
-
+    
 </script>
 
 <style>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,28 +1,31 @@
 <template>
     <div class="person">
-        <h1>中国</h1>
-        <h2 ref="location">重庆</h2>
-        <h3>重科</h3>
-        <button @click="consoleH2">输出H2</button>
+        ???
     </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
-
-    let location = ref();
-
-    let a = 0;
-    let b = 1;
-    let c = 2;
-
-    function consoleH2(){
-        /* <h2 data-v-4cadc14e="">重庆</h2> */
-        /* 其中data-v-4cadc14e= 是style中scoped标签作用，用于隔离样式 */
-        console.log(location.value);
-    }
-
-    defineExpose({a, b, c})
+    import {type PersonInter, type Persons} from '@/types'
+    // let person:PersonInter = {
+    //     id: '123123',
+    //     name: '123123',
+    //     age: 100,
+    // }
+    let personList:Persons = [
+    {
+        id: '1',
+        name: '1',
+        age: 1,
+    },{
+        id: '2',
+        name: '2',
+        age: 100,
+    },{
+        id: '3',
+        name: '3',
+        age: 10,
+    },
+    ]
 </script>
 
 <style scoped>
