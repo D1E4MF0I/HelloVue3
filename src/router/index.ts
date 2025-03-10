8e59db1f5caee0add9a5ceb531789927ce235ae9
DreamFor <dreamforway@qq.com>
2025-03-10 16:25:13 +0800

    feat(42): 路由_重定向
    路由配置中添加配置即可

--- src/router/index.ts
+++ src/router/index.ts
@@ -51,6 +51,10 @@ const router = createRouter({
             path:'/about',
             component:About,
         },
+        {
+            path:'/',
+            redirect:'/home'
+        },
     ]
 })
 
