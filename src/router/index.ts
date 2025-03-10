3edda7316f39c0189ce98b837424986eadecb300
DreamFor <dreamforway@qq.com>
2025-03-10 11:32:27 +0800

    feat(33): 路由_路由器两种工作模式
    1.history模式，URL更美观；需要服务端配合处理路径问题
    2.hash模式，兼容性更好；URL不美观，SEO优化较差

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,11 +4,12 @@
 import About from "@/pages/About.vue";
 import Home from "@/pages/Home.vue";
 import News from "@/pages/News.vue";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, createWebHashHistory } from "vue-router";
 
 // 创建路由器
 const router = createRouter({
-    history:createWebHistory(),/* 路由器的工作模式 */
+    // history:createWebHistory(),/* 路由器的工作模式 */
+    history:createWebHashHistory(),/* 路由器的工作模式 */
     routes:[
         {
             path:'/home',
