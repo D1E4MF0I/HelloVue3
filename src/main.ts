d32e61184711d9b9291049bfa32abec7d940ae5e
DreamFor <dreamforway@qq.com>
2025-03-10 18:07:08 +0800

    feat(45): 搭建pinia环境

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import {createApp} from 'vue'
 import App from './App.vue'
+// 引入pinia
+import { createPinia } from 'pinia'
 
-// 创建一个应用
+// 第一步 创建一个应用
 const app = createApp(App)
+// 第二步 创建pinia
+const pinia = createPinia()
+// 第三步 安装pinia
+app.use(pinia)
 app.mount('#app')
